665fcfd514b461e2a1d8ac4451feed8c3c310bcc
nelialanets <neliarod@stanford.edu>
2022-06-23 22:49:16 -0700

    set up react routs, pages, navbar, react-routerodom, components(basic)

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Layout() {
+  return (
+    <>
+             <hr style={{color:"black"}}></hr>
+      <div>
+        <h1>Landing Page</h1>
+      </div>
+             
+   </>
+  )
+}
