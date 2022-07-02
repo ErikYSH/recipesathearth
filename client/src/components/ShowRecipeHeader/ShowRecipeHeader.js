fd60ddd1c17dec7e52ba191cb12afc8cb0c24c02
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-07-01 18:15:59 -0700

    ShowRecipeHeader component added CSS styling

--- client/src/components/ShowRecipeHeader/ShowRecipeHeader.js
+++ client/src/components/ShowRecipeHeader/ShowRecipeHeader.js
@@ -1,6 +1,9 @@
+import './index.css'
+
 export default function ShowRecipeHeader(props) {
     return (
-        <div className="recipe_header_container">
+        <div className="header_info">
+            <div className='recipe_header_container'>
             <div className="recipe_header_img">
                 <img src={props.recipe_header_img} alt="header_img"/>
                 {/* {props.recipe_header_img} */}
@@ -8,9 +11,13 @@ export default function ShowRecipeHeader(props) {
             <div className="recipe_header_info">
                 <h1>{props.recipe_header_name}</h1>
                 <h2>{props.recipe_header_category}</h2>
-                <h3>{props.recipe_header_prep_time}</h3>
-                <h3>{props.recipe_header_portion}</h3>
+                <hr />
+                <h3>Prep Time: {props.recipe_header_prep_time}</h3>
+                <h3>Portion: {props.recipe_header_portion}</h3>
             </div>
+            </div>
+            <hr />
         </div>
+     
     )
 }
